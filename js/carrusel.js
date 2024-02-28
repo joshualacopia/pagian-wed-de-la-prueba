c9e830734a05dee96fbada0ffc3dba5915d45b1c
joshuareynoso <josetellamo201@gmail.com>
2024-02-27 22:12:10 -0300

    arreglo al carrusel
    e modificado que al para el mouse se detenga el carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,15 +2,21 @@ const carrusel = document.querySelector('.carrusel-ites');
 let maxscrollLeft =carrusel.scrollWidth - carrusel.clientWidth
 let intervalo = null;
 let paso = 1;
-const start = () => {
-intervalo = setInterval(function () {
-    carrusel.scrollLeft = carrusel.scrollLeft + paso;
-    if (carrusel.scrollLeft  === maxscrollLeft){
-        paso = paso * -1;
-    }  else if (carrusel.scrollLeft === 0){
-        paso =paso * -1;
-    }
-    },10)
+const stop = () => {
+    clearInterval(intervalo);
 };
+const start = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollLeft = carrusel.scrollLeft + paso;
+        if (carrusel.scrollLeft === maxscrollLeft) {
+            paso = paso * -1;
+        } else if (carrusel.scrollLeft === 0) {
+            paso = paso * -1;
+        }
+    }, 10);
 
+    carrusel.addEventListener('mouseenter', stop);
+
+    carrusel.addEventListener('mouseleave', start);
+};
 start();
